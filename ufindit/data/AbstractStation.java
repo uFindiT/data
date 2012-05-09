5036baa2c0e33af1477b956035b5e8fa56e1c254
Areusus <Daniel Reichmann@DanielReichmann.(none)>
2012-05-09 14:22:13 +0200

    Interface

--- ufindit/data/AbstractStation.java
+++ ufindit/data/AbstractStation.java
@@ -1,5 +1,26 @@
 package ufindit.data;
 
 public interface AbstractStation {
-
+	//-name: String
+	/**
+	 * -String name
+	 * - list<Station, Integer> nachf(ALLE nachfolgerstationen werden gespeichert)
+	 * - Station vorg (Der Vorgänger von dem gekommen wird 
+	 * - Int Duration
+	 * - boolean locked
+	 * - boolean umstieg
+	 * - Strign linie
+	 * @param a
+	 */
+	public void changePrevious(AbstractStation a);
+	public void setDuration(int t); 
+	public int getDuration();
+	public void lock(); //Setzt Attribut lock auf true!
+	public boolean isLocked();
+	public void isUmstieg();//Prüft ob vom Vorgänger zu dieser Station sich die line(String) geändert hat. Wenn ja wird Umstieg auf true gesetzt!
+	public String getLinie();//returnt linie;
+	/**
+	 * Im Konstruktor werden die ganzen Attribute aus der Datenbank gelesen. 
+	 * Vorg. wird am Anfang null gelassen. locked ist false umstieg ist false ges. ist 10000!
+	 */
 }
